9a7f3864ef55f23c11936a9ce28963a6b14634f7
Anıl Engin Keretli <116742352+anlkeretlii@users.noreply.github.com>
2024-11-28 20:24:37 +0300

    Add files via upload

---
+++ csharpkamp_02/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpkamp_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region double degiskener
+            //double number;
+            //number = 4.85;
+            //Console.WriteLine(number);
+
+            Console.WriteLine("--------Fiyat listesi");
+            Console.WriteLine();
+            double applePrice, orangePrice, strawberryPrice, potato, tomato;
+
+            applePrice = 14.85;
+            orangePrice = 20.95;
+            strawberryPrice = 45;
+            potato = 9.74;
+            tomato = 6.88;
+            Console.WriteLine("elma biirm fiyatı" + applePrice);
+            Console.WriteLine("portakal biirm fiyatı" + orangePrice);
+            Console.WriteLine("strawberry biirm fiyatı" + strawberryPrice);
+            Console.WriteLine("patates biirm fiyatı" + potato);
+            Console.WriteLine("domates biirm fiyatı" + tomato);
+
+            //not tutmaya çok üştendim ciddili başlangıcın başlangıcı
+
+
+
+
+
+
+            Console.Read();
+
+            #endregion
+
+        }
+    }
+}
