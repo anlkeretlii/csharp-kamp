a55f73072e1e80a5d29ee99b9acde4a0a459f576
Anıl Engin Keretli <116742352+anlkeretlii@users.noreply.github.com>
2024-11-28 20:34:17 +0300

    Add files via upload

---
+++ csharpkamp_02/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpkamp_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region double degiskener
+            //double number;
+            //number = 4.85;
+            //Console.WriteLine(number);
+
+            Console.WriteLine("--------Fiyat listesi");
+            Console.WriteLine();
+            double applePrice, orangePrice, strawberryPrice, potato, tomato;
+
+            applePrice = 14.85;
+            orangePrice = 20.95;
+            strawberryPrice = 45;
+            potato = 9.74;
+            tomato = 6.88;
+            Console.WriteLine("elma biirm fiyatı" + applePrice);
+            Console.WriteLine("portakal biirm fiyatı" + orangePrice);
+            Console.WriteLine("strawberry biirm fiyatı" + strawberryPrice);
+            Console.WriteLine("patates biirm fiyatı" + potato);
+            Console.WriteLine("domates biirm fiyatı" + tomato);
+
+            //not tutmaya çok üştendim ciddili başlangıcın başlangıcı
+
+            #region char veriables
+
+       
+
+            char symbol;
+            symbol = 'a';
+
+            Console.WriteLine(symbol);
+
+            #endregion
+
+            #region STRING VARIABLE DATA INPUTS | STRING DEĞİŞKEN VERİ GİRİŞLERİ
+
+            //Console.WriteLine("Hava Yolları Yolcu Bilgileri");
+            //Console.WriteLine();
+
+            //string passengerIdentityNumber, passengerName, passengerSurname, passengerAge, passengerDistrict, passengerCity;
+
+            //Console.Write("Kimlik No");
+            //passengerIdentityNumber = Console.ReadLine();
+
+            //Console.Write("Adı:");
+            //passengerName = Console.ReadLine();
+
+            //Console.Write("Soyadı:");
+            //passengerSurname = Console.ReadLine();
+
+            //Console.Write("Yaş:");
+            //passengerAge = Console.ReadLine();
+
+            //Console.Write("İlçe:");
+            //passengerDistrict = Console.ReadLine();
+
+            //Console.Write("Şehir:");
+            //passengerCity = Console.ReadLine();
+
+            //Console.WriteLine();
+            //Console.WriteLine("-------------------------------");
+            //Console.WriteLine();
+            //Console.WriteLine("Kimlik No: " + passengerIdentityNumber + " | Yolcu Ad Soyad: " + passengerName + " " + passengerSurname + " | " + passengerAge + " | " + passengerDistrict + "/" + passengerCity);
+            //Console.WriteLine();
+            //Console.WriteLine("-------------------------------");
+
+            #endregion
+
+            #region int data donusumleri
+
+            //int shoesPrice, computerPrice, chairPrice, tvPrice;
+
+            //shoesPrice = 1000;
+            //computerPrice = 20000;
+            //chairPrice = 5000;
+            //tvPrice = 12000;
+
+            //int shoesCount, computerCount, chairCount, tvCount;
+
+            //Console.Write("Lütfen aldığınız ayakkabı sayısını giriniz: ");
+            //shoesCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Lütfen aldığınız bilgidayar sayısını giriniz: ");
+            //computerCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Lütfen aldığınız sandalye sayısını giriniz: ");
+            //chairCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Lütfen aldığınız televizyon sayısını giriniz: ");
+            //tvCount = int.Parse(Console.ReadLine());
+
+            //int totalPrice = shoesCount * shoesPrice + computerCount * computerPrice + chairCount * chairPrice + tvCount * tvPrice;
+
+            //Console.WriteLine();
+            //Console.WriteLine("Toplam Ödemeniz Gereken Tutar: " + totalPrice);
+
+            #endregion
+
+            #region double degisken girsi
+
+            //double exam1, exam2, exam3, result;
+
+            //Console.Write("1. Sınav Notunu Giriniz: ");
+            //exam1 = double.Parse(Console.ReadLine());
+
+            //Console.Write("2. Sınav Notunu Giriniz: ");
+            //exam2 = double.Parse(Console.ReadLine());
+
+            //Console.Write("3. Sınav Notunu Giriniz: ");
+            //exam3 = double.Parse(Console.ReadLine());
+
+            //result = (exam1 + exam2 + exam3) / 3;
+            //Console.WriteLine();
+            //Console.WriteLine("Sınav Ortalamanız: " + result);
+
+            #endregion
+
+            #region char degisken girisleri
+
+            //char gender;
+            //Console.Write("Lütfen cinsiyet seçiniz: ");
+            //gender = char.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Seçtiğiniz Cinsiyet: " + gender);
+
+            #endregion
+
+
+
+
+
+
+            Console.Read();
+
+            #endregion
+
+        }
+    }
+}
